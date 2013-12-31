d8d3395129384d79a20885aa21e54d6a18d23e01
Ryan Blair <rblair@gmail.com>
2013-12-31 05:41:40 +0000

    initial script to stack checkouts

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git clone \
+  https://github.com/omniti-labs/omnios-build --branch template \
+  omnios-build
+
+#sooooon
+# git clone \
+#   https://github.com/...... --branch ..... \
+#   omnios-build/build
+
+# if we have a customized site.sh -- note this is in .gitignore
+if [ -f config/site.sh ]; then
+  cp config/site.sh omnios-build/lib/site.sh
+else # we only have the example copy
+  cp config/site.sh.dist omnios-build/lib/site.sh
+fi
+
